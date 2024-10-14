651e363535ece30b281a6b638bc0da78fd92aa98
samuelmsilva2v <samuelmsilva@outlook.com.br>
2024-10-14 15:00:46 -0300

    added routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { CadastrarEnderecoComponent } from './cadastrar-endereco/cadastrar-endereco.component';
+import { ConsultarEnderecosComponent } from './consultar-enderecos/consultar-enderecos.component';
+import { EditarEnderecoComponent } from './editar-endereco/editar-endereco.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'app/cadastrar-endereco',
+        component: CadastrarEnderecoComponent
+    },
+    {
+        path: 'app/consultar-endereco',
+        component: ConsultarEnderecosComponent
+    },
+    {
+        path: 'app/editar-endereco',
+        component: EditarEnderecoComponent
+    },
+];
