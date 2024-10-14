4ea2e95dbe6c013bb3e5fa34561fbb62496a8139
samuelmsilva2v <samuelmsilva@outlook.com.br>
2024-10-14 16:07:09 -0300

    consultar enderecos working

--- src/app/consultar-enderecos/consultar-enderecos.component.ts
+++ src/app/consultar-enderecos/consultar-enderecos.component.ts
@@ -1,12 +1,31 @@
+import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-consultar-enderecos',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './consultar-enderecos.component.html',
   styleUrl: './consultar-enderecos.component.css'
 })
 export class ConsultarEnderecosComponent {
 
+  enderecos: any[] = [];
+
+  constructor(
+    private httpClient: HttpClient
+  ) {}
+
+  ngOnInit() {
+
+    this.httpClient.get(
+      'http://localhost:8080/api/enderecos'
+    ).subscribe({
+      next: (resposta) => {
+        this.enderecos = resposta as any[];
+      }
+  })
+
+  }
 }
