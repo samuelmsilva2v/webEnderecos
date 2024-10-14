ae543087243b9430f31567995ac7d29c179ec1a3
samuelmsilva2v <samuelmsilva@outlook.com.br>
2024-10-14 15:49:56 -0300

    cadastrar endereco working

--- src/app/cadastrar-endereco/cadastrar-endereco.component.ts
+++ src/app/cadastrar-endereco/cadastrar-endereco.component.ts
@@ -1,12 +1,44 @@
+import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-cadastrar-endereco',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule, ReactiveFormsModule],
   templateUrl: './cadastrar-endereco.component.html',
   styleUrl: './cadastrar-endereco.component.css'
 })
 export class CadastrarEnderecoComponent {
 
+  mensagem: string = '';
+
+  constructor(
+    private httpClient: HttpClient
+  ) {}
+
+  formulario = new FormGroup({
+    logradouro: new FormControl('', [Validators.required]),
+    numero: new FormControl('', [Validators.required]),
+    complemento: new FormControl('', [Validators.required]),
+    bairro: new FormControl('', [Validators.required]),
+    cidade: new FormControl('', [Validators.required]),
+    uf: new FormControl('', [Validators.required]),
+    cep: new FormControl('', [Validators.required])
+  })
+
+  cadastrarEndereco(){
+
+    this.httpClient.post(
+      'http://localhost:8080/api/enderecos',
+      this.formulario.value,
+      {responseType: 'text'}
+    ).subscribe({
+      next: (resposta) => {
+        this.mensagem = resposta;
+        this.formulario.reset()
+      }
+    })
+  }
 }
